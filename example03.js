dcedb9a5cb6282e9f821325cdcb9f95cda33e173
wchampionjohn <vhitotoyouv@gmail.com>
2018-01-11 13:56:58 +0800

    initialize

--- example03.js
+++ example03.js
@@ -1,11 +1,14 @@
 var app = angular.module('app', []);
 
 app.controller('indexCtrl', function($scope) {
-  $scope.items = [{title:'Iphone 8', price: 30000, quantity: 1}, {title:'Iphone X', price: 60000, quantity:1}]
+  $scope.items = [
+    {title:'Iphone 8', price: 30000, quantity: 1},
+    {title:'Iphone X', price: 60000, quantity:1}
+  ]
 
   $scope.getTotal = function(){
-    return $scope.items.reduce( function(a, b){
-        return a + b.price * b.quantity;
+    return $scope.items.reduce( function(result, item){
+        return result + item.price * item.quantity;
     }, 0);
   }
 })
