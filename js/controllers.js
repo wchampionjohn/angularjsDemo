30527be299f9f4c578a7654da2249e845af34bcc
wchampionjohn <vhitotoyouv@gmail.com>
2018-01-11 09:57:31 +0800

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+function TodoCrtlRemovable($scope) {
+  $scope.newItem = '';
+  $scope.todoList = [];
+  $scope.addItem = function(){
+    if(this.newItem){
+      this.todoList.push({label:this.newItem,isFinish:false});
+      this.newItem = '';
+    }
+  }
+  $scope.removeItem = function(item){
+    item.isFinish = true;
+  }
+}
